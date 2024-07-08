1ad5123a8bad318def6410013314f2bdc6922e59
Ailec1 <126810380+Ailec1@users.noreply.github.com>
2024-07-08 08:30:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,15 +54,15 @@ while true; do
         ;;
     4)
         rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
-        cat > /etc/yum.repos.d/wazuh.repo << EOF
-        [wazuh]
-        gpgcheck=1
-        gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
-        enabled=1
-        name=EL-\$releasever - Wazuh
-        baseurl=https://packages.wazuh.com/4.x/yum/
-        protect=1
-        EOF
+        # cat > /etc/yum.repos.d/wazuh.repo << EOF
+        # [wazuh]
+        # gpgcheck=1
+        # gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+        # enabled=1
+        # name=EL-\$releasever - Wazuh
+        # baseurl=https://packages.wazuh.com/4.x/yum/
+        # protect=1
+        # EOF
         WAZUH_MANAGER="10.0.70.2" yum install wazuh-agent
         systemctl daemon-reload
         systemctl enable wazuh-agent
